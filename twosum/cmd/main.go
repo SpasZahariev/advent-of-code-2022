81a9f2cb40538e84604d11809c6f339f82f8ad12
SpasZahariev <spaszahariev54@gmail.com>
2022-11-26 10:39:12 +0000

    adding a small solution to twosum

--- twosum/cmd/main.go
+++ twosum/cmd/main.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
+type void struct{} // use void as a syntax suger for empty structure
+var member void    // a variable we can reuse for the void type
+
 func main() {
-	fmt.Println("Hello, world!")
+
+	// asdfasdfasdf asdfasdfa asdfasd
+
+	target := 27
+	fmt.Println("We are looking for X and Y that will make: ", target)
+	input := []int{12, 25, 15, 14, 45}
+
+	set := make(map[int]void)
+
+	for _, value := range input {
+		set[target-value] = member
+	}
+
+	var isAnswered bool = false
+	for _, item := range input {
+		_, isAnswered = set[item]
+		if isAnswered {
+			fmt.Println("We have a winner!: ", item, " + ", target-item)
+			break
+		}
+
+	}
+
+	// just in case we didn't find anything
+	if !isAnswered {
+		fmt.Println("We could not make a two sum to hit the target!")
+	}
 }
